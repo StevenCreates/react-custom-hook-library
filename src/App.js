4826a6ce57ac0efc1f08b9f43d012ce090bd32fb
StevenCreates <steven@stevencreates.tech>
2020-11-13 20:38:57 -0700

    added input to update use document title hook

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import { useDocumentTitle } from "./hooks/useDocumentTitle";
 
 function App() {
+  const [newTitle, setNewTitle] = useState("");
+  const updateTitle = (event) => {
+    setNewTitle(event.target.value);
+  };
+  useDocumentTitle(newTitle);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <input onChange={updateTitle} type='text' />
     </div>
   );
 }
