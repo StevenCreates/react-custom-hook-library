abcd7906dd20605c5095152c905c390e732c3a44
StevenCreates <steven@stevencreates.tech>
2020-11-13 21:16:17 -0700

    added html validation checks to test validation hook

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import "./App.css";
 import { useDocumentTitle } from "./hooks/useDocumentTitle";
+import { useValidate } from "./hooks/useValidate";
 
 function App() {
   const [newTitle, setNewTitle] = useState("");
@@ -8,9 +9,47 @@ function App() {
     setNewTitle(event.target.value);
   };
   useDocumentTitle(newTitle);
+  const [firstPassword, setFirstPassword] = useState("");
+  const [secondPassword, setSecondPassword] = useState("");
+
+  const [validLength, hasNumber, upperCase, lowerCase, match] = useValidate({
+    firstPassword: firstPassword,
+    secondPassword: secondPassword,
+  });
+
+  const setFirst = (event) => {
+    setFirstPassword(event.target.value);
+  };
+  const setSecond = (event) => {
+    setSecondPassword(event.target.value);
+  };
+
   return (
     <div className='App'>
       <input onChange={updateTitle} type='text' />
+
+      <div>
+        Passwords
+        <input onChange={setFirst} type='text' />
+        <input onChange={setSecond} type='text' />
+      </div>
+      <div>
+        <ul>
+          <li>
+            Valid Length: {validLength ? <span>True</span> : <span>False</span>}
+          </li>
+          <li>
+            Has a Number: {hasNumber ? <span>True</span> : <span>False</span>}
+          </li>
+          <li>
+            UpperCase: {upperCase ? <span>True</span> : <span>False</span>}
+          </li>
+          <li>
+            LowerCase: {lowerCase ? <span>True</span> : <span>False</span>}
+          </li>
+          <li>Match: {match ? <span>True</span> : <span>False</span>}</li>
+        </ul>
+      </div>
     </div>
   );
 }
