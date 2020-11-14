551c5704bfd42669280235194f553cb789e69e56
StevenCreates <steven@stevencreates.tech>
2020-11-13 21:15:52 -0700

    created a validation hook to accept two passwords

---
+++ src/hooks/useValidate.js
@@ -0,0 +1,19 @@
+import { useState, useEffect } from "react";
+
+export const useValidate = ({ firstPassword = "", secondPassword = "" }) => {
+  const [validLength, setValidLength] = useState(null);
+  const [hasNumber, setHasNumber] = useState(null);
+  const [upperCase, setUpperCase] = useState(null);
+  const [lowerCase, setLowerCase] = useState(null);
+  const [match, setMatch] = useState(null);
+
+  useEffect(() => {
+    setValidLength(firstPassword.length >= 8 ? true : false);
+    setHasNumber(/\d/.test(firstPassword));
+    setUpperCase(firstPassword.toLowerCase() !== firstPassword);
+    setLowerCase(firstPassword.toUpperCase() !== firstPassword);
+    setMatch(firstPassword && firstPassword === secondPassword);
+  }, [firstPassword, secondPassword]);
+
+  return [validLength, hasNumber, upperCase, lowerCase, match];
+};
